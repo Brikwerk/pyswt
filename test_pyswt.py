0c58c8c6eccd19fc63354910d18abe86e5f97376
Brikwerk <reece@brikwerk.com>
2019-03-22 18:33:19 -0700

    Added output writeout code

--- test_pyswt.py
+++ test_pyswt.py
@@ -1,9 +1,7 @@
 import pyswt
 import cv2
 
-img_path = "./images/swt-example.png"
+img_path = "./images/swt-example-2.png"
 img = cv2.imread(img_path)
 swt_img = pyswt.run(img)
-cv2.imshow('image',swt_img)
-cv2.waitKey(0) # Waiting for any keypress when focused on displayed image window
-cv2.destroyAllWindows()
+cv2.imwrite("output.png", swt_img)
