5d92cf998e5b82570f7265681e0433c975e71837
Brikwerk <reece@brikwerk.com>
2019-04-02 01:19:49 -0700

    Added optional parameters for diagnostic output

--- test_pyswt.py
+++ test_pyswt.py
@@ -9,7 +9,7 @@ img_path = "./images/" + img_name + ext
 img_output_path = "./output/"
 
 img = cv2.imread(img_path)
-final_img, swt_ld, cc_ld, cc_boxes, filtered_cc = pyswt.run(img)
+final_img, swt_ld, cc_ld, cc_boxes, filtered_cc = pyswt.run(img, diagnostic=True, timing=True)
 
 # Creating output directory if not available
 if not os.path.exists(img_output_path):
@@ -22,4 +22,3 @@ cv2.imwrite(img_output_path + img_name + "-cc-bw-light" + ext, cc_ld[0])
 cv2.imwrite(img_output_path + img_name + "-cc-bw-dark" + ext, cc_ld[1])
 cv2.imwrite(img_output_path + img_name + "-cc-BB" + ext, cc_boxes)
 cv2.imwrite(img_output_path + img_name + "-cc-filtered" + ext, filtered_cc)
-
